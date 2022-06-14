328759edd32fbfaf76bbddf7e0bda2873688b1c0
Maxime <maxouf304@gmail.com>
2022-06-14 20:03:29 +0200

    Add initVisitor + print visitor + variables

--- main.c
+++ main.c
@@ -6,6 +6,12 @@
 #include <stdlib.h>
 #include <semaphore.h>
 
+#define PATIENCE_MAX 30
+#define PATIENCE_MIN 10
+
+#define MONEY_MAX 1000
+#define MONEY_MIN 200
+
 struct visitor
 {
     int money;
@@ -19,8 +25,44 @@ struct attraction
     // Beware semaphore inside
 };
 
+int initRandom(int max, int min)
+{
+    return rand()%(max-min) + min;
+}
+
+void initVisitor(struct visitor visitors[], int n) 
+{
+    int i;
+    srand(time(NULL));
+    
+    for(i = 0; i < n; i++)
+    {
+        visitors[i].money = initRandom(MONEY_MAX, MONEY_MIN);
+        visitors[i].patience = initRandom(PATIENCE_MAX, PATIENCE_MIN);
+    }
+}
+
+void printVisitor(struct visitor vis)
+{
+    printf("Money : %d\n", vis.money);
+    printf("Patience : %d\n", vis.patience);
+}
+
 int main()
 {
+    
+    //Initialization
+    int nbVisitors = 20;
+    struct visitor visitors[nbVisitors];
+    int i;
+    
+    initVisitor(visitors, nbVisitors);
+    
+    for(i = 0; i < nbVisitors; i++)
+    {
+        printVisitor(visitors[i]);
+    }
+    
     printf("EHEHEHAH\n");
 
     return 0;
